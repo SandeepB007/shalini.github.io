532ed778268bb52b92bd29ff080942d5c0eb5fb7
SandeepB007 <ballasandeep877@gmail.com>
2023-11-14 14:21:17 +0530

    Adding new messages in letter.js

--- letter.js
+++ letter.js
@@ -1,7 +1,17 @@
 var i = 0;
 var txt1 = `
-YO Sandeeeep, >>
-Let's start here !!!!
+Yo Shaliniiiii, >>
+I think we are meeting again at the same place !!!! >>>
+
+I am very bad at expressing myself and very good at messing things, dont know what to say where to start. -> <-// Emoji >>>
+
+I just want you to know that you are my special special speciallllllll person and will always be.
+It might not be for you, but naaku any small thing you do affects me a lotttt both in good and bad ways. >>>
+
+Will you come on a "Date" with me 😍.// love-emoji.
+
+// option to select Yes or No.
+
 `
 var speed = 50;
 typeWriter();
