f37b1bb8f5f69da67b85702a4024963b80e70867
SandeepB007 <ballasandeep877@gmail.com>
2023-11-14 15:13:33 +0530

    Added letter with indentations

--- letter.js
+++ letter.js
@@ -5,12 +5,12 @@ Yo Shaliniiiii, >>>
 This is a special letter for you ! >>>
 Don't worry this time, you can simply hide this from anyone 😉.>>>
 
-I am very bad at expressing myself and very good at messing things, dont know what to say where to start 👉👈. >>
-Whenever I see you, forget seeing even when I talk with you. I feel fullll butterflies and em cheppalo anni marchiptotaaa 😅😁. >>>
-I just want you to know that you are my special special speciallllllll person and will always be. >>
+I am very bad at expressing myself and very good at messing things, dont know what to say where to start 👉👈. >
+Whenever I see you, forget seeing even when I talk with you. I feel fullll butterflies and em cheppalo anni marchiptotaaa 😅😁. >
+I just want you to know that you are my special special speciallllllll person and will always be. >
 I want to call you my cutiepie, sweetie, bangaram, bujji, bajji, ammulu everything. I want to make you feel as special as you are in my head. >>>
 
-Are you ready to feel special !! >>>
+Are you ready to feel special !! >
 Will you come on a "First Date" with me 😍.
 `
 var speed = 50;
